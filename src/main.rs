569844529f7aa3d08b0e6e66991ef683c282650a
Mahdi <mtzarrin@gmail.com>
2025-01-27 18:37:18 +0330

    27-01-2025

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use chrono::{DateTime, Local};
-use leptos::{either::Either, prelude::*};
-use reactive_stores::{OptionStoreExt as _, Store, StoreFieldIterator};
-use reactive_stores_macro::Store;
+use leptos::{either::Either, leptos_dom::logging::console_log, prelude::*};
+use reactive_stores::{Field, OptionStoreExt as _, Store};
 use serde::{Deserialize, Serialize};
 use web_sys::MouseEvent;
 
@@ -28,6 +27,7 @@ pub struct Note {
 fn App() -> impl IntoView {
   let state = Store::new(State::default());
   let selected_note = Store::new(<Option<Note>>::None);
+
   if let Some(first_note) = state.notes().get().first() {
     selected_note.set(Some(first_note.to_owned()));
   }
@@ -37,37 +37,34 @@ fn App() -> impl IntoView {
     state.notes().update(|it| it.insert(0, new_note.clone()));
     selected_note.set(Some(new_note));
   };
-  let delete_note = move |child: Note| {
+  let delete_note = move |remove_index: Signal<usize>, child: Field<Note>| {
     move |event: MouseEvent| {
       event.stop_propagation();
-
-      if selected_note.get().is_some_and(|it| it.date == child.date) {
-        match state.notes().get().as_slice() {
+      if selected_note.read().as_ref().is_some_and(|it| it.date == *child.date().read()) {
+        match state.notes().read().as_slice() {
           [_single_note] => selected_note.set(None),
-          [.., before_last_note, last_note] if last_note.date == child.date => {
+          [.., before_last_note, last_note] if last_note.date == *child.date().read() => {
             selected_note.set(Some(before_last_note.to_owned()))
           }
-          _ => {
+          notes => {
             selected_note.set(
-              state
-                .notes()
-                .get()
+              notes
                 .windows(2)
-                .find(|window| window[0].date == child.date)
+                .find(|window| window[0].date == *child.date().read())
                 .map(|window| window[1].to_owned()),
             );
           }
         }
       }
       request_animation_frame(move || {
-        state.notes().update(|it| it.retain(|item| item.date != child.date));
+        state.notes().write().remove(remove_index.get());
       });
     }
   };
   let update_selected_note_title = move |event| {
     state
       .notes()
-      .iter()
+      .into_iter()
       .filter(|note| note.get().date == selected_note.unwrap().get().date)
       .next()
       .unwrap()
@@ -78,7 +75,7 @@ fn App() -> impl IntoView {
   let update_selected_note_content = move |event| {
     state
       .notes()
-      .iter()
+      .into_iter()
       .filter(|note| note.get().date == selected_note.unwrap().get().date)
       .next()
       .unwrap()
@@ -86,7 +83,6 @@ fn App() -> impl IntoView {
       .set(event_target_value(&event));
     selected_note.unwrap().content().set(event_target_value(&event)); // For Updating the selected item in the list
   };
-
   view! {
     <div id="main-div">
       <div id="list">
@@ -94,69 +90,78 @@ fn App() -> impl IntoView {
           "ADD NEW NOTE"
         </button>
         <ul>
-          <For each=move || state.notes() key=move |note| note.date().get() let:child>
+          <ForEnumerate
+            each=move || state.notes()
+            key=move |note| note.date().get()
+            let(index,
+            child)
+          >
             <li
               class="note-item new-item"
               class:selected=move || {
-                  selected_note.get().is_some_and(|it| { it.date == child.get().date })
+                selected_note.get().is_some_and(|it| { it.date == child.get().date })
               }
               on:click=move |_| selected_note.set(Some(child.get()))
             >
               <div class="items">
-                <div class="title">{move || child.title().get()}</div>
+                <div class="title">
+                  {move || format!("{} {}", child.title().get(), index.get())}
+                </div>
                 <div class="content">{move || child.content().get()}</div>
               </div>
-              <button on:click=delete_note(child.get()) class="fa fa-trash delete-button"></button>
+
+              <button
+                on:click=delete_note(index.into(), child.into())
+                class="fa fa-trash delete-button"
+              ></button>
             </li>
-          </For>
+          </ForEnumerate>
         </ul>
       </div>
 
       <div id="editor">
         {move || match selected_note.get() {
-            Some(selected_note) => {
-                Either::Right(
-                    view! {
-                      <textarea
-                        id="title-editor"
-                        rows="2"
-                        prop:value=selected_note.title
-                        on:input=update_selected_note_title
-                      ></textarea>
-                      <textarea
-                        id="content-editor"
-                        prop:value=selected_note.content
-                        on:input=update_selected_note_content
-                      ></textarea>
-                    },
-                )
-            }
-            None => {
-                Either::Left(
-                    view! { <div style="margin:auto; font-size:21px;">"Pick a note"</div> },
-                )
-            }
+          Some(selected_note) => {
+            Either::Right(
+              view! {
+                <textarea
+                  id="title-editor"
+                  rows="2"
+                  prop:value=selected_note.title
+                  on:input=update_selected_note_title
+                ></textarea>
+                <textarea
+                  id="content-editor"
+                  prop:value=selected_note.content
+                  on:input=update_selected_note_content
+                ></textarea>
+              },
+            )
+          }
+          None => {
+            Either::Left(view! { <div style="margin:auto; font-size:21px;">"Pick a note"</div> })
+          }
         }}
 
       </div>
     </div>
   }
 }
-#[component]
-pub fn ShowSome<N, T, EF>(
-  children: EF,
-  #[prop(into)] option: Store<Option<T>>,
-  #[prop(optional, into)] fallback: ViewFn,
-) -> impl IntoView
-where
-  N: IntoView + 'static,
-  T: Clone + 'static + Send + Sync + PartialEq,
-  EF: Fn(T) -> N + 'static + Send + Sync,
-{
-  let memoized_when = Memo::new_owning(move |_| (option.get(), option.with(Option::is_some)));
+// #[component]
+// pub fn ShowSome<N, T, EF>(
+//   children: EF,
+//   #[prop(into)] option: Store<Option<T>>,
+//   #[prop(optional, into)] fallback: ViewFn,
+// ) -> impl IntoView
+// where
+//   N: IntoView + 'static,
+//   T: Clone + 'static + Send + Sync + PartialEq,
+//   EF: Fn(T) -> N + 'static + Send + Sync,
+// {
+//   let memoized_when = Memo::new_owning(move |_| (option.get(), option.with(Option::is_some)));
 
-  move || match memoized_when.get() {
-    Some(value) => Either::Right(children(value).into_view()),
-    None => Either::Left(fallback.run()),
-  }
-}
+//   move || match memoized_when.get() {
+//     Some(value) => Either::Right(children(value).into_view()),
+//     None => Either::Left(fallback.run()),
+//   }
+// }
